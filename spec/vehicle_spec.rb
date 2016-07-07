59de4b26c9e4f50a7254a83ceea9e2b5cc4c8137
Halle Williams and Maldon Meehan <maldon@>
2016-07-07 10:03:34 -0700

    return false if the car is not American made and less that 15 years old failing spec test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -55,4 +55,11 @@ end
       expect(test_vehicle.age()).to(eq(16))
     end
   end
+
+  describe("#worth_buying?") do
+    it("returns false if the car is not American made and less that 15 years old") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.worth_buying?()).to(eq(false))
+    end
+  end
 end
