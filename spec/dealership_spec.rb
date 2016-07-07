4568da74208b86211198602df22264398ae780a3
Halle Williams and Maldon Meehan <halle@>
2016-07-07 13:07:46 -0700

    return the name of the dealership passing test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -4,8 +4,8 @@ require('dealership')
 describe(Dealership) do
 
   describe('#name') do
-    it("returns the namne of the dealership") do
-      test_dealership = Dealership.new("Bob's Use Cars")
+    it("returns the name of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
       expect(test_dealership.name()).to(eq("Bob's Used Cars"))
     end
   end
