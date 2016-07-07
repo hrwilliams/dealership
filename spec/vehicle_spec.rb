9d13c9099f1c157ff871ed28f5051235684b52fe
Halle Williams and Maldon Meehan <halle@>
2016-07-07 09:07:29 -0700

    returns the model of the vehicle failing test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -6,9 +6,18 @@ describe('Vehicle') do
     # Vehicle.clear()
 end
 
-  describe("#make")
+  describe("#make") do
     it("returns the make of the vehicle") do
       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
       expect(test_vehicle.make()).to(eq("Toyota"))
     end
+  end
+
+  describe("#model") do
+    it("returns the model of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.model()).to(eq("Prius"))
+    end
+  end
+
 end
