38c54a6e2b4b61dfc56dcd9649ea51a36b034cdb
Halle Williams and Maldon Meehan <maldon@>
2016-07-07 13:26:21 -0700

    add a dealership to the array of saved dealerships failing spec test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -3,24 +3,33 @@ require('dealership')
 
 describe(Dealership) do
 
-  describe('#name') do
+  describe("#name") do
     it("returns the name of the dealership") do
       test_dealership = Dealership.new("Bob's Used Cars")
       expect(test_dealership.name()).to(eq("Bob's Used Cars"))
     end
   end
 
-  describe('#id') do
+  describe("#id") do
     it ("returns the id of the dealership") do
       test_dealership = Dealership.new("Bob's Used Cars")
       expect(test_dealership.id()).to(eq(1))
     end
   end
 
-  describe('#cars') do
+  describe("#cars") do
     it("initially returns an empty array of cars for the dealership") do
       test_dealership = Dealership.new("Bob's Used Cars")
       expect(test_dealership.cars()).to(eq([]))
     end
   end
+
+  describe("#save") do
+    it("adds a dealership to the array of saved dealerships") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      test_dealership.save()
+      expect(Dealership.all()).to(eq([test_dealership]))
+    end
+  end
+  
 end
