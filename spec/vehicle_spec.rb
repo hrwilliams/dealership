df261f6c8dafd0f2e07e9b9710b1e44e5ce18c22
Halle Williams and Maldon Meehan <halle@>
2016-07-07 09:59:55 -0700

    return the vehicles age passing test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -52,7 +52,7 @@ end
   describe("#age") do
     it("returns the vehicles age") do
       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
-      expect(test_vehicle.age()).to(eq(15))
+      expect(test_vehicle.age()).to(eq(16))
     end
   end
 end
